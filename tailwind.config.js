94195eefb8b4fca11633148fb2cabcbf76997402
Nabil Woalid Rafiu <nabilwoalidrafiu@gmail.com>
2023-02-25 10:48:16 +0600

    index.html page

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+          inter: ['Inter', 'sans-serif'],
+          poppins: ['Poppins', 'sans-serif'],
+          roboto: ['Roboto', 'sans-serif']
+      }
+    },
+    colors: {
+      h1Color: "#143E4A",
+      h2Color: "#4C4C4C",
+      bodyText:  '#646464',
+      primary :  '#D1FED1',
+      button : '#008D96',
+      gradient: '#CDE5E0',
+      jobs: "#D8E7FF"
+    }
+  },
+  plugins: [],
+}
