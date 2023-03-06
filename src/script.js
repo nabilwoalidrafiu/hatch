968f57b2f6d6c0a11cd50d968536a62d64cd8560
Nabil Woalid Rafiu <nabilwoalidrafiu@gmail.com>
2023-03-06 09:50:03 +0600

    acordion

--- src/script.js
+++ src/script.js
@@ -1,46 +1,32 @@
-
-       
-
-const dropdown = document.getElementById('dropdown');
-// dropdown.style.display = 'hidden';
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
+jQuery(document).ready(function(){
+   jQuery('.accordion-item > button > svg.upAC').hide();
+        jQuery('.accordion > .accordion-item > .accordion-content').hide();
+          jQuery('.accordion-item').each(function(){
+              jQuery(this).on('click', function(){
+                jQuery(this).find('.accordion-title').toggleClass('font-semibold');
+                jQuery(this).find('svg.upAC').toggle();
+                jQuery(this).find('svg.downAC').toggle();
+                jQuery(this).find('.accordion-content').slideToggle();
+              })
+            }
+          )
+            
+        const dropdown = document.getElementById('dropdown');
+        dropdown.style.display = 'hidden';
+          window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var i;
+              for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+                }
+              }
+            }
+          }
+   })
+   function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
-   jQuery(document).ready(function(){
-   
-  
-        jQuery('.accordion > .accordion-item > .accordion-content').hide();
-        jQuery('.chevron-up').hide();
-        jQuery('.chevron-down').show();
-        jQuery('.accordion-item').on('click', function(){
-          if (jQuery(this).hasClass("active")) {
-            jQuery(this).removeClass("active").find(".accordion-content").slideUp();
-            
-          } else {
-            jQuery(".accordion > .accordion-item.active > .accordion-content").slideUp();
-            jQuery(".accordion > .accordion-item.active").removeClass("active");
-            jQuery(this).addClass("active").find(".accordion-content").slideDown();
-            
-          }
-          return false;
-        })
-   })
