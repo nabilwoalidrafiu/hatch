861f53d76e5a3c44f7976e16eccb1a06dedb4285
Nabil Woalid Rafiu <nabilwoalidrafiu@gmail.com>
2023-03-08 13:41:37 +0600

    candidate-my profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,20 @@ module.exports = {
           inter: ['Inter', 'sans-serif'],
           poppins: ['Poppins', 'sans-serif'],
           roboto: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        h1Color: "#143E4A",
+        h2Color: "#4C4C4C",
+        h3Color: "#273240",
+        bodyText:  '#646464',
+        primary :  '#D1FED1',
+        button : '#008D96',
+        gradient: '#CDE5E0',
+        jobs: "#D8E7FF",
+        
       }
     },
-    colors: {
-      h1Color: "#143E4A",
-      h2Color: "#4C4C4C",
-      h3Color: "#273240",
-      bodyText:  '#646464',
-      primary :  '#D1FED1',
-      button : '#008D96',
-      gradient: '#CDE5E0',
-      jobs: "#D8E7FF"
-    }
+   
   },
   plugins: [],
 }
