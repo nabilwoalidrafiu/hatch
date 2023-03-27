7fdd28ecf960e37ee2eee4e3a72398918c6c0322
Nabil Woalid Rafiu <76890924+nabilwoalidrafiu@users.noreply.github.com>
2023-03-27 08:43:52 +0600

    index.html resonsive

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 jQuery(document).ready(function(){
+  jQuery('#menu_bar').hide()
+  jQuery('.toggle_btn').click(function(){
+    jQuery('#menu_bar').animate({width: 'toggle'});
+  })
    jQuery('.accordion-item > button > svg.upAC').hide();
         jQuery('.accordion > .accordion-item > .accordion-content').hide();
           jQuery('.accordion-item').each(function(){
@@ -10,7 +14,12 @@ jQuery(document).ready(function(){
               })
             }
           )
-            
+          jQuery('.forCandidate').click(function() {
+            jQuery('.candidateDropdown').toggleClass("show")
+          })
+          jQuery('.forEmployer').click(function() {
+            jQuery('.employerDropdown').toggleClass("show")
+          })
         const dropdown = document.getElementById('dropdown');
         dropdown.style.display = 'hidden';
           window.onclick = function(event) {
@@ -25,8 +34,11 @@ jQuery(document).ready(function(){
               }
             }
           }
+          
    })
+   
    function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
+ 
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,28 @@ module.exports = {
         jobs: "#D8E7FF",
         iconBg: '#D9D9D9',
         lightText: '#777777'
-      }
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1000px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1366px',
+        // => @media (min-width: 1366px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+        '3xl': '1920px',
+        // => @media (min-width: 1920px) { ... }
+      },
+      container: {
+        center: true,
+      },
     },
    
   },
